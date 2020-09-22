211b0856767d5b650ca0cf101096c9f742288b9c
Guru Priya V <gurupv24@in.ibm.com>
2020-09-22 19:10:09 +0530

    adding test project

---
+++ src/test/java/FirstTry/test_demo/AppTest.java
@@ -0,0 +1,23 @@
+package FirstTry.test_demo;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void simpleTest()
+    {
+    	int a = 1;
+    	int b = 2;
+    	
+    	assertTrue( a+b == 3 );
+    }
+}
